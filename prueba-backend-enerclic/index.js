fbc0a558bb970d1a72a60b4dd5a6d3ddc2cfed95
rivi <javivarro@msn.com>
2022-07-14 14:07:57 +0200

    backend code refactorized

--- prueba-backend-enerclic/index.js
+++ prueba-backend-enerclic/index.js
@@ -5,12 +5,12 @@ const express = require("express");
 const app = express();
 
 //importamos las rutas
-const relatives = require("./routes/relatives");
+const dataRoutes = require("./routes/dataRoutes");
 
 //Middleware que comprueba si el contenido es del tipo application/json
-app.use(express.json());
+app.use(express.json('type'));
 
 //ruta base para los endpoints
-app.use("/relatives", relatives);
+app.use("/", dataRoutes);
 
 app.listen(process.env.PORT, () => console.log("Listening on port", process.env.PORT));
